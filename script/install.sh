bb78ad65847891da7f7452f369b3e1eef3dcf94d
Stian Eklund <stian.eklund@gmail.com>
2017-01-17 16:14:06 +0100

    add install options to script, install zsh, tmux, emacs, vim & spacemacs

--- script/install.sh
+++ script/install.sh
@@ -7,7 +7,7 @@
 
 dir=~/.dotfiles
 olddir=~/.dotfiles_old
-files=".gitconfig .vim .oh-my-zsh .zsh .vimrc .viminfo .vimtags .dir_colors .tmux.conf .minttyrc .githelpers .lesskey .zshrc .tmux.conf .pentadactylrc"
+files=".gitconfig .vim .oh-my-zsh .zsh .vimrc .viminfo .vimtags .dir_colors .tmux.conf .minttyrc .githelpers .zshrc .tmux.conf .spacemacs .profile"
 
 ##########
 
@@ -29,13 +29,62 @@ for file in $files; do
     ln -s $dir/$file ~/$file
 done
 
-# grab Vundle from github
-echo "Grabbing Vundle for you.."
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-echo "Grabbing oh-my-zsh.."
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-echo "Running Vim :PluginInstall"
-vim +PluginInstall +qall
+# Install Vim?
+read -p "Install Vim? 1: apt, 2: pkg-ng, n/N " option
+echo
+case "$option" in
+    1 ) echo "apt"; sudo apt install vim;;
+    2 ) echo "pkg"; sudo pkg install vim;;
+    n|N ) echo "No";;
+    * ) echo "Invalid option";;
+esac
+# Install Vim plugins?
+read -p "Install Vim plugins & oh-my-zsh? Y/n " option
+echo
+case "$option" in
+    y|Y ) echo "Yes";
+        echo "Installing Vundle";
+        git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim;
+        echo "Installing oh-my-zsh";
+        git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh;
+        vim +PluginInstall +qall;;
+    n|n ) echo "No";;
+    * ) echo "Invalid option";;
+esac
+# Install Zsh & tmux?
+read -p "Install Zsh & Tmux? 1: apt, 2: pkg-ng n/N " option
+echo
+case "$option" in
+    1 ) echo "Apt"; sudo apt install zsh tmux;;
+    2) echo "Pkg"; sudo pkg install zsh tmux;;
+    n|n ) echo "No";;
+    * ) echo "Invalid option";;
+esac
+# Install Emacs?
+read -p "Install Emacs? 1: Apt, 2: pkg-ng, n/N " choice
+echo
+case "$choice" in
+    1 ) echo "apt"; sudo apt install emacs;;
+    2 ) echo "pkg"; sudo pkg install emacs;;
+    n|N ) echo "No";;
+    * ) echo "Invalid option";;
+esac
 
+# Install Spacemacs?
+read -p "Install Spacemacs? y/n " option
+echo
+case "$option" in
+    y|Y ) echo "Yes"; git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d;;
+    n|N ) echo "No";;
+    * ) echo "Invalid option";;
+esac
 
-
+# Install i3?
+read -p "Install i3? 1: Apt, 2: pkg-ng, n/N " option
+echo
+case "$option" in
+    1) echo "Apt"; sudo apt install i3 i3blocks;;
+    2) echo "pkg"; sudo pkg install i3 i3blocks;;
+    n|N ) echo "No";;
+    * ) echo "Invalid option";;
+esac
